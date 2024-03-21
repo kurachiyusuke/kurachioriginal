6ec6c58d5ac109787c65934deec2b139c0c35b81
kurachiyusuke <137065115+kurachiyusuke@users.noreply.github.com>
2024-03-21 23:21:11 +0900

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,39 @@
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    private static Map<String,Boolean> componentHarmfulMap = Map.of("果糖ブドウ糖液糖",false,"ぶどう果実",false,"エストラジオール",true);
+    private static Map<String,Boolean> componentHarmfulMap = new HashMap<>();
     public static void main(String[] args) {
+        componentHarmfulMap.put("果糖ブドウ糖液糖",false);
+        componentHarmfulMap.put("ぶどう果実",false);
+        componentHarmfulMap.put("エストラジオール",true);
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
         if(componentHarmfulMap.containsKey(name)){
-            hantei(args);
+            hantei(name);
         }
         else{
             String harmfulOrNot = sc.next();
             if(harmfulOrNot.equals("有害")){
                 componentHarmfulMap.put(name,true);
-                hantei(args);
+                hantei(name);
             }
             if(harmfulOrNot.equals("無害")){
                 componentHarmfulMap.put(name,false);
-                hantei(args);
+                hantei(name);
             }
         }
     }
-    public static void hantei(String[] args2){
-        for(String s : args2) {
-            Boolean isHarmful = componentHarmfulMap.get(s);
+    private static void hantei(String name){
+            Boolean isHarmful = componentHarmfulMap.get(name);
             if(isHarmful) {
                 System.out.println("有害です");
-                return;
             }
-        }
-        System.out.println("無害です");
+            else{
+                System.out.println("無害です");
+            }
     }
 }
